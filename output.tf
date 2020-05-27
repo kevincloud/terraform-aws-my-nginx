c4e4cfb334655d90d29195204e011295d0cc807b
Kevin Cochran <kcochran@hashicorp.com>
2020-05-27 19:45:08 -0400

    add variables

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "public_ip" {
-    value = aws_instance.aws_instance.jp-k8s-master.public_ip.public_ip
+    value = aws_instance.module_instance.public_ip
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,6 @@
 variable "identifier" { }
+variable "key_pair" { }
+variable "instance_size" { }
 variable "profile_id" { }
 variable "sg_id" { }
 variable "subnet_id" { }
